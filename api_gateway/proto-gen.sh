4baf5902d7ec12fd9613d24cb5cedc2fbe635fbd
DoniyorAli <doniyorfarhodov96@gmail.com>
2022-12-13 18:12:21 +0500

    Api_gateway submodule updated

--- api_gateway/proto-gen.sh
+++
@@ -1,7 +0,0 @@
-protoc --go_out=./protogen \
-    --go-grpc_out=./protogen \
-    ./protoc/article/*.proto
-
-protoc --go_out=./protogen \
-    --go-grpc_out=./protogen \
-    ./protoc/author/*.proto
